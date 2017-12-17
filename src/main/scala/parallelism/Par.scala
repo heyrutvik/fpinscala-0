806653e2befca8e944df955ac53d24252aefb07a
Rutvik <heyrutvik@gmail.com>
2017-12-18 00:15:52 +0530

    function for Par type

--- src/main/scala/parallelism/Par.scala
+++ src/main/scala/parallelism/Par.scala
@@ -1,20 +1,41 @@
 package parallelism
 
-trait Par[+A]
+import java.util.concurrent._
 
 object Par {
 
-  def unit[A](a: => A): Par[A] = ???
+  type Par[A] = ExecutorService => Future[A]
 
-  def get[A](a: Par[A]): A = ???
+  private case class UnitFuture[A](get: A) extends Future[A] {
+    def isDone = true
+    def get(timeout: Long, units: TimeUnit) = get
+    def isCancelled = false
+    def cancel(evenIfRunning: Boolean): Boolean = false
+  }
 
-  def map2[A,B,C](p1: Par[A], p2: Par[B])(f: (A, B) => C): C = ???
+  def unit[A](a: A): Par[A] = _ => UnitFuture(a)
 
-  def sum(as: IndexedSeq[Int]): Int = {
-    if (as.size <= 1) as.headOption getOrElse 0
+  def map2[A,B,C](p1: Par[A], p2: Par[B])(f: (A, B) => C): Par[C] = { s =>
+    val f1 = p1(s)
+    val f2 = p2(s)
+    unit(f(f1.get, f2.get))(s)
+  }
+
+  def fork[A](a: => Par[A]): Par[A] = { s =>
+    s.submit(() => a(s).get())
+  }
+
+  def async[A](a: => A): Par[A] = fork(unit(a))
+
+  def run[A](s: ExecutorService)(a: Par[A]): Future[A] = a(s)
+
+  def asyncF[A,B](f: A => B): A => Par[B] = a => async(f(a))
+
+  def sum(as: IndexedSeq[Int]): Par[Int] = {
+    if (as.size <= 1) unit(as.headOption getOrElse 0)
     else {
       val (l, r) = as.splitAt(as.length / 2)
-      Par.map2(unit(sum(l)), unit(sum(r)))(_ + _)
+      Par.map2(sum(l), sum(r))(_ + _)
     }
   }
 }
