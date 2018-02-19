07b354edd2ae2d934610cb75717f9b201b0a6021
RR <heyrutvik@gmail.com>
2018-02-19 18:51:41 +0530

    Gen functions

--- src/main/scala/testing/Gen.scala
+++ src/main/scala/testing/Gen.scala
@@ -1,14 +1,21 @@
 package testing
 
 import functionalstate.{Action, RNG}
+import laziness._
+
+case class Gen[+A](sample: Action[RNG, A], exhaustive: Stream[A])
 
 object Gen {
 
-  type Gen[A] = Action[RNG, A]
+  def unit[A](a: => A): Gen[A] = Gen(Action.unit(a), Stream(a))
 
-  def listOf[A](a: Gen[A]): Gen[List[A]] = ???
+  def boolean: Gen[Boolean] = Gen(Action.unit(false), Stream(false, true))
 
-  def listOfN[A](n: Int, a: Gen[A]): Gen[List[A]] = ???
+  def choose(start: Int, stopExclusive: Int): Gen[Int] = {
+    Gen(
+      Action(RNG.positiveInt1).map(i => start + i % (stopExclusive - start)),
+      Stream(start until stopExclusive: _*))
+  }
 
-  def choose(start: Int, stopExclusive: Int): Gen[Int] = Action(RNG.int).map(i => i % stopExclusive)
+  def listOfN[A](n: Int, g: Gen[A]): Gen[List[A]] = ???
 }
