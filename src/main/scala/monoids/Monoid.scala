58923c1e14fc003d0ab1a4ea6d8707f51822bc28
RR <heyrutvik@gmail.com>
2018-03-12 16:53:56 +0530

    code added

--- src/main/scala/monoids/Monoid.scala
+++ src/main/scala/monoids/Monoid.scala
@@ -4,50 +4,65 @@ import testing.Prop
 
 trait Monoid[A] {
   def op(a1: A, a2: A): A
+
   def zero: A
 }
 
 object Monoid {
 
-  val stringMonoid = new Monoid[String] {
+  implicit val stringMonoid = new Monoid[String] {
     override def op(a1: String, a2: String): String = a1 + a2
+
     override def zero: String = ""
   }
 
   def listMonoid[A] = new Monoid[List[A]] {
     override def op(a1: List[A], a2: List[A]): List[A] = a1 ++ a2
+
     override def zero: List[A] = Nil
   }
 
   val intAddition = new Monoid[Int] {
     override def op(a1: Int, a2: Int): Int = a1 + a2
+
     override def zero: Int = 0
   }
 
   val intMultiplication = new Monoid[Int] {
     override def op(a1: Int, a2: Int): Int = a1 * a2
+
     override def zero: Int = 1
   }
 
   val booleanOr = new Monoid[Boolean] {
     override def op(a1: Boolean, a2: Boolean): Boolean = a1 || a2
+
     override def zero: Boolean = false
   }
 
   val booleanAnd = new Monoid[Boolean] {
     override def op(a1: Boolean, a2: Boolean): Boolean = a1 && a2
+
     override def zero: Boolean = true
   }
 
   def optionMonoid[A] = new Monoid[Option[A]] {
     override def op(a1: Option[A], a2: Option[A]): Option[A] = a1 orElse a2
+
     override def zero: Option[A] = None
   }
 
   def endoMonoid[A] = new Monoid[A => A] {
     override def op(a1: A => A, a2: A => A): A => A = a1 compose a2
+
     override def zero: A => A = x => x
   }
 
   def monoidLaws[A](m: Monoid[A]): Prop = ???
+
+  def concatenate[A](as: List[A])(implicit m: Monoid[A]): A = as.foldLeft(m.zero)(m.op)
+
+  def foldMap[A, B](as: List[A], m: Monoid[B])(f: A => B): B = as.map(f).foldLeft(m.zero)(m.op)
+
+  def foldLeft[A, B](as: List[A], z: B)(f: (B, A) => B): B = ???
 }
